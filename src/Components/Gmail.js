cf6b8c2285effdfc58edba1a68f82f033d2818c0
ari-sarkar <arisarkar1995@gmail.com>
2021-01-25 18:06:29 +0530

    error fixing

--- src/Components/Gmail.js
+++ src/Components/Gmail.js
@@ -1,15 +1,22 @@
 import React, { useEffect } from "react";
 
 const Gmail = ({ acessToken, email }) => {
-    console.log(acessToken,"ACT")
   useEffect(() => {
-    fetch(`https://gmail.googleapis.com/gmail/v1/users/arisarkar.mail@gmail.com/messages`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'access_token': 'ya29.a0AfH6SMCJewlO_tsoNlfaZ0RvdZFSc06fH6dXRQmeXo0RSpLYBp0gMMxyfT0nu1nd6kZKXSjodRxzKhH7No59VHDSrOQ457XfIMnpXlbf6h4y_e9So5vLHU09vzxyv7Zf6FusR-YYwIGWiW5yltZ4pUe-SVj7QTV6-277anwNMdk'
-      },
-    })
-      .then(res => console.log(res, "response"))
+    fetch(
+      `https://gmail.googleapis.com/gmail/v1/users/arisarkar.mail@gmail.com/messages`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          'Access_token':
+            "ya29.a0AfH6SMCJewlO_tsoNlfaZ0RvdZFSc06fH6dXRQmeXo0RSpLYBp0gMMxyfT0nu1nd6kZKXSjodRxzKhH7No59VHDSrOQ457XfIMnpXlbf6h4y_e9So5vLHU09vzxyv7Zf6FusR-YYwIGWiW5yltZ4pUe-SVj7QTV6-277anwNMdk",
+          'Authorization':
+            "Bearer ya29.a0AfH6SMCJewlO_tsoNlfaZ0RvdZFSc06fH6dXRQmeXo0RSpLYBp0gMMxyfT0nu1nd6kZKXSjodRxzKhH7No59VHDSrOQ457XfIMnpXlbf6h4y_e9So5vLHU09vzxyv7Zf6FusR-YYwIGWiW5yltZ4pUe-SVj7QTV6-277anwNMdk",
+        },
+      }
+    )
+      .then(res => res.json())
+      .then(re => console.log(re,"result"))
       .catch(err => console.log(err, "error"));
   });
   return <div></div>;
--- src/Components/GoogleLogin.js
+++ src/Components/GoogleLogin.js
@@ -9,16 +9,13 @@ const GoogleLoginPage = () => {
   const [imgUrl, setImgUrl] = useState("");
   const [acessToken, setAcessToken] = useState("");
   const responseGoogle = response => {
-    console.log(response);
     setLogin(true);
     setUserName(response.profileObj.name);
     setEmail(response.profileObj.email);
     setImgUrl(response.profileObj.imageUrl);
     setImgUrl(response.profileObj.imageUrl);
-    // setAcessToken(response.tokenObj.access_token);
-    setAcessToken(response.tokenId);
+    setAcessToken(response.tokenObj.access_token);
   };
-  console.log("new update")
   return (
     <section id="login">
       {login ? (
