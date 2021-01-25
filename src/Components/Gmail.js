619dce06c6611c70b1a7ecd34d20a04ec4a50243
ari-sarkar <arisarkar1995@gmail.com>
2021-01-25 17:55:08 +0530

    error fixing

--- src/Components/Gmail.js
+++ src/Components/Gmail.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 const Gmail = ({ acessToken, email }) => {
     console.log(acessToken,"ACT")
   useEffect(() => {
-    fetch(`https://gmail.googleapis.com/gmail/v1/users/${email}/messages`, {
+    fetch(`https://gmail.googleapis.com/gmail/v1/users/arisarkar.mail@gmail.com/messages`, {
       headers: {
         'Content-Type': 'application/json',
         'access_token': 'ya29.a0AfH6SMCJewlO_tsoNlfaZ0RvdZFSc06fH6dXRQmeXo0RSpLYBp0gMMxyfT0nu1nd6kZKXSjodRxzKhH7No59VHDSrOQ457XfIMnpXlbf6h4y_e9So5vLHU09vzxyv7Zf6FusR-YYwIGWiW5yltZ4pUe-SVj7QTV6-277anwNMdk'
