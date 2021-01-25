a77a6fe97e72a72f22b12f8a8a455f263cf243dc
ari-sarkar <arisarkar1995@gmail.com>
2021-01-25 18:31:21 +0530

    error fixing

--- src/Components/Gmail.js
+++ src/Components/Gmail.js
@@ -6,7 +6,7 @@ const Gmail = ({ acessToken, email }) => {
       `https://gmail.googleapis.com/gmail/v1/users/arisarkar.mail@gmail.com/messages`,
       {
         method: "GET",
-        mode : "no-cors",
+        Host: 'gmail.googleapis.com',
         headers: {
           "Content-Type": "application/json",
           Access_token:
