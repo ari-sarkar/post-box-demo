d0c9760b0932907c1bb514de909a80065ac588fc
ari-sarkar <arisarkar1995@gmail.com>
2021-01-25 19:20:56 +0530

    error fixing

--- src/Components/Gmail.js
+++ src/Components/Gmail.js
@@ -6,14 +6,11 @@ const Gmail = ({ acessToken, email }) => {
       `https://gmail.googleapis.com/gmail/v1/users/arisarkar.mail@gmail.com/messages`,
       {
         method: "GET",
-        credentials: 'include',
         headers: {
           "Content-Type": "application/json",
-          Access_token:
-            "ya29.a0AfH6SMCJewlO_tsoNlfaZ0RvdZFSc06fH6dXRQmeXo0RSpLYBp0gMMxyfT0nu1nd6kZKXSjodRxzKhH7No59VHDSrOQ457XfIMnpXlbf6h4y_e9So5vLHU09vzxyv7Zf6FusR-YYwIGWiW5yltZ4pUe-SVj7QTV6-277anwNMdk",
-          Authorization:
-            "Bearer ya29.a0AfH6SMCJewlO_tsoNlfaZ0RvdZFSc06fH6dXRQmeXo0RSpLYBp0gMMxyfT0nu1nd6kZKXSjodRxzKhH7No59VHDSrOQ457XfIMnpXlbf6h4y_e9So5vLHU09vzxyv7Zf6FusR-YYwIGWiW5yltZ4pUe-SVj7QTV6-277anwNMdk",
-            'Origin': 'https://happy-jennings-422b37.netlify.app'
+          Access_token: acessToken,
+          Authorization: `Bearer ${acessToken}`,
+          Origin: "https://happy-jennings-422b37.netlify.app",
         },
       }
     )
