c5c49a0060f0473dafe4618a1f0a6d693cdb026c
ari-sarkar <arisarkar1995@gmail.com>
2021-01-25 17:19:50 +0530

    error fixing

--- src/Components/Gmail.js
+++ src/Components/Gmail.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 const Gmail = ({ acessToken, email }) => {
-useEffect(() => {
-    fetch(`https://gmail.googleapis.com/gmail/v1/users/${email}/messages`,{
-        headers: {
-            acess_token :  acessToken
-        }
-    }).then(res => console.log(res,"response"))
-    .catch(err => console.log(err, "error"))
-})
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  useEffect(() => {
+    fetch(`https://gmail.googleapis.com/gmail/v1/users/${email}/messages`, {
+      headers: {
+        acess_token: acessToken,
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Headers": "Authorization",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "https://mail.google.com",
+      },
+    })
+      .then(res => console.log(res, "response"))
+      .catch(err => console.log(err, "error"));
+  });
+  return <div></div>;
+};
 
-export default Gmail
+export default Gmail;
