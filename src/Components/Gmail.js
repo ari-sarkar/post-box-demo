e2084dbe2faee08baac9d806f479f86e57fc6582
ari-sarkar <arisarkar1995@gmail.com>
2021-01-25 19:28:01 +0530

    error fixing

--- src/Components/Gmail.js
+++ src/Components/Gmail.js
@@ -3,14 +3,15 @@ import React, { useEffect } from "react";
 const Gmail = ({ acessToken, email }) => {
   useEffect(() => {
     fetch(
-      `https://gmail.googleapis.com/gmail/v1/users/arisarkar.mail@gmail.com/messages`,
+      `https://gmail.googleapis.com/gmail/v1/users/${email}/messages`,
       {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
           Access_token: acessToken,
           Authorization: `Bearer ${acessToken}`,
-          Origin: "https://happy-jennings-422b37.netlify.app",
+          'Access-Control-Allow-Origin': "https://happy-jennings-422b37.netlify.app",
+          
         },
       }
     )
